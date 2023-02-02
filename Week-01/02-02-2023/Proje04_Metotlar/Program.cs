f8bfae0ce6a00cd2f2a742463c9e916c4955c420
mehmettalbayrak <112020000+mehmettalbayrak@users.noreply.github.com>
2023-02-02 22:08:44 +0300

    Proje04_Metotlar

---
+++ Week-01/02-02-2023/Proje04_Metotlar/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Proje04_Metotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region MetinselMetotlar
+            string metin = "Bahçeşehir Üniversitesi";
+            int uzunluk = metin.Length; //Aslında Lenght bir metot değil.
+            Console.WriteLine("\"" + metin + "\"" + " metninin uzunluğu " + uzunluk + " \\karakterdir.\\");
+            Console.WriteLine($"{metin} metninin uzunluğu {uzunluk} karakterdir.");
+
+            #endregion
+        }
+    }
+}
