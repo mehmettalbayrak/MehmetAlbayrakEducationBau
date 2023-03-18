f7c745be80b4e8f556d264db69ed713a2e960761
mehmettalbayrak <112020000+mehmettalbayrak@users.noreply.github.com>
2023-03-18 11:52:56 +0300

    Yemek Öncesi

---
+++ Week-06/05-JavaScript/18-03-2023/03-ToDoAppV2/app.js
@@ -0,0 +1,70 @@
+"use strict"
+
+const inputTaskName = document.querySelector("#input-new-task");
+const btnNewTask = document.querySelector("#btn-new-task");
+const btnClear = document.querySelector("#btn-clear");
+const taskList = document.querySelector("#tasks-list");
+
+let isEditMode = false; //Kullanıcının o anda yeni kayıt mı yoksa düzenleme modunda mı olduğunu burada kontrol edeceğiz. Eğer bu false ise yeni kayıt modundayız (ekle). True ise düzeltme modundayız.
+let editedTaskId; //O anda hangi görevi düzenliyoruz bilgisini uygulama boyunca kullanmak için tanımlandı.
+
+let taskListArray = [
+    //Örnek data gireceğimiz dizi. Veri kaynağımız. İleride local storage'de ne yazacaksa buraya dolduracağız. Yani boş kalacak. Burayı kendi storage'in olarak düşünebilirsin memo.
+    { "id": 1, "name": "Spotify faturasını öde", "status": "pending" },
+    { "id": 2, "name": "Digiturk faturasını öde", "status": "pending" },
+    { "id": 3, "name": "Turknet faturasını öde", "status": "completed" },
+    { "id": 4, "name": "Netflix faturasını öde", "status": "pending" },
+    { "id": 5, "name": "Disney+ faturasını öde", "status": "pending" }
+];
+
+btnNewTask.addEventListener("click", addNewTask); //addEventListener bu buttona tıklanabilir ya da bir eylem yapılabilir gibi durumlarda kullanılır.("tıklanınca demek olur", daha önce yazılan ya da burada yazılan functionı koyarız. Ve bu fonksiyon tıklanınca çalışır.)
+btnClear.addEventListener("click", clearAll);
+
+function addNewTask() {
+
+}
+
+function clearAll() {
+
+}
+
+function displayTasks() {
+    //Bu function HER İHTİYAÇ duyulduğunda tüm görevleri yeniden ekranda göstermek için kullanılacak.
+    taskList.innerHTML = ""; //Bu komut html'de yazılı olan bilgileri sayfadan siler. Bu örnekte li lerimiz.
+    if (taskListArray.length === 0) {
+        taskList.innerHTML = '<div class="alert">Burada görev bulunmamaktadır!</div>'
+    }
+    for (let task of taskListArray) { //taskListArray'de dönüp dönen elemanları task yapar.
+        let completed = task.status == "completed" ? "checked" : ""; //task status eğer completed ise checked yap değilse boş bırak.
+        let taskLi = `
+            <li class="task-container">
+          <div class="task-header">
+            <input onclick="updateStatus(this);" class="task-check" type="checkbox" id="${task.id}" ${completed}>
+            <input id = "_${task.id}" class="task-input ${completed}" type="text" value="${task.name}" disabled>
+          </div>
+          <div class="btn-group">
+            <button class="btn-edit">
+              Düzenle
+            </button>
+            <button class="btn-delete">
+              Sil
+            </button>
+          </div>
+        </li> 
+        `;
+        taskList.insertAdjacentHTML("beforeend", taskLi) //Her eklenen yeni görevin nereye ekleneceğini belirleriz. Before end eklenecek yer olarak belirtilen elementin kapanış taginin öncesine eklemesini sağlar. beforeend gibi 3 tane daha var bunlar da başlangıçtan sonra ya da önce ya da kapanıştan sonra ya da önce.
+    }
+}
+
+
+function updateStatus(selectedTask) {
+    let updatedTaskId;
+    let newStatus = selectedTask.checked ? "completed" : "pending";
+    for (const i in taskListArray) {
+        if (selectedTask.id == taskListArray[i].id) {
+            taskListArray[i].status = newStatus;
+        }
+    }
+    displayTasks();
+}
+displayTasks();
