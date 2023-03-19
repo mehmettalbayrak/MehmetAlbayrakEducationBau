70c33b4a509c671b37efbe76e4787602d8569db8
mehmettalbayrak <112020000+mehmettalbayrak@users.noreply.github.com>
2023-03-19 14:44:02 +0300

    sliderok

---
+++ Week-06/05-JavaScript/19-03-2023/04_Modal/app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const btnOpenModal = document.querySelector("#btn-open-modal");
+const mainModal = document.querySelector (".main-modal");
+const btnCloseModal = document.querySelector(".btn-close-modal");
+
+btnOpenModal.addEventListener("click", function(){
+    mainModal.classList.remove("main-modal-none");
+});
+
+btnCloseModal.addEventListener("click", closeModal);
+
+mainModal.addEventListener("click", function(e){
+    if(e.target.className == "main-modal") {
+        closeModal();
+    }
+})
+
+function closeModal() {
+    mainModal.classList.add("main-modal-none");
+}
---
+++ Week-06/05-JavaScript/19-03-2023/05_Slider/app.js
@@ -0,0 +1,64 @@
+const slides = document.querySelectorAll(".slide");
+const btnPrev = document.querySelector("#prev");
+const btnNext = document.querySelector("#next");
+const headerSpan = document.querySelector(".header h1 span");
+
+
+let autoSlider = true;
+let sliderInterval;
+let timerInterval;
+let counter;
+let intervalTime = 5000;
+
+btnPrev.addEventListener("click", function () {
+    prevSlide();//Bu bir önceki slidea geçişi sağlayan func
+    againInterval();
+});
+
+btnNext.addEventListener("click", function () {
+    nextSlide();//Bu bir sonraki slidea geçişi sağlayan func
+    againInterval();
+});
+
+function prevSlide() {
+    let activeSlide = document.querySelector(".active");
+    activeSlide.classList.remove("active");
+    if (activeSlide.previousElementSibling) {
+        activeSlide.previousElementSibling.classList.add("active");
+    } else {
+        slides[slides.length - 1].classList.add("active");
+    }
+}
+
+function nextSlide() {
+    let activeSlide = document.querySelector(".active");
+    activeSlide.classList.remove("active");
+    if (activeSlide.nextElementSibling && activeSlide.nextElementSibling.classList.contains("slide")) {
+        activeSlide.nextElementSibling.classList.add("active");
+    } else {
+        slides[0].classList.add("active");
+    }
+}
+
+function againInterval() {
+    if (autoSlider) {
+        clearInterval(sliderInterval);
+        clearInterval(timerInterval);
+        counter = 1;
+        headerSpan.innerText = `(${counter})`;
+        sliderInterval = setInterval(nextSlide, intervalTime);
+        timerInterval = setInterval(timerDisplay, 1000);
+    }
+};
+
+if (autoSlider) {
+    againInterval();
+}
+
+function timerDisplay() {
+    counter++;
+    headerSpan.innerText = `(${counter})`;
+    if (counter > 5) againInterval();
+}
+
+//Zamanı başlığın yanında gösterin!
