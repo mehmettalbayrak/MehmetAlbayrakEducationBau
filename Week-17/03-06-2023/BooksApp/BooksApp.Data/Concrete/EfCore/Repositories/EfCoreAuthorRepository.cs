57605f8849a455b91b41e3392b216e0662ed90c4
mehmettalbayrak <112020000+mehmettalbayrak@users.noreply.github.com>
2023-06-03 14:46:41 +0300

    admin silinmiş araçlar

---
+++ Week-17/03-06-2023/BooksApp/BooksApp.Data/Concrete/EfCore/Repositories/EfCoreAuthorRepository.cs
@@ -0,0 +1,40 @@
+﻿using BooksApp.Data.Abstract;
+using BooksApp.Data.Concrete.EfCore.Contexts;
+using BooksApp.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksApp.Data.Concrete.EfCore.Repositories
+{
+    public class EfCoreAuthorRepository:EfCoreGenericRepository<Author>, IAuthorRepository
+    {
+        BooksAppContext _context = new BooksAppContext();
+
+        public async Task CreateWithUrl(Author author)
+        {
+            await _context.Authors.AddAsync(author);
+            await _context.SaveChangesAsync();
+            author.Url = author.Url + "-" + author.Id;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Author>> GetAllAuthorsAsync(bool isDeleted, bool? isActive = null)
+        {
+            var result = _context
+                .Authors
+                .Where(a => a.IsDeleted == isDeleted)
+                .AsQueryable();
+            if (isActive != null)
+            {
+                result = result
+                    .Where(a => a.IsActive == isActive)
+                    .AsQueryable();
+            }
+            return await result.ToListAsync();
+        }
+    }
+}
---
+++ Week-17/03-06-2023/BooksApp/BooksApp.MVC/Areas/Admin/Models/BookListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BooksApp.MVC.Areas.Admin.Models
+{
+    public class BookListViewModel
+    {
+        public List<BookViewModel> BookViewModelList { get; set; }
+
+        public string SourceAction { get; set; }
+    }
+}
