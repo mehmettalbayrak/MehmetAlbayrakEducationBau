8911c37e1daac9ae79e5198074c8984178ac5734
mehmettalbayrak <112020000+mehmettalbayrak@users.noreply.github.com>
2023-06-04 11:55:01 +0300

    Genel Yazar/Yayınevi

---
+++ Week-17/04-06-2023/BooksApp/BooksApp.Data/Concrete/EfCore/Repositories/EfCoreAuthorRepository.cs
@@ -0,0 +1,53 @@
+﻿using BooksApp.Data.Abstract;
+using BooksApp.Data.Concrete.EfCore.Contexts;
+using BooksApp.Entity.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksApp.Data.Concrete.EfCore.Repositories
+{
+    public class EfCoreAuthorRepository:EfCoreGenericRepository<Author>, IAuthorRepository
+    {
+        BooksAppContext _context = new BooksAppContext();
+
+        public async Task CreateWithUrl(Author author)
+        {
+            await _context.Authors.AddAsync(author);
+            await _context.SaveChangesAsync();
+            author.Url = author.Url + "-" + author.Id;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Author>> GetAllAuthorsAsync(bool isDeleted, bool? isActive = null)
+        {
+            var result = _context
+                .Authors
+                .Where(a => a.IsDeleted == isDeleted)
+                .AsQueryable();
+            if (isActive != null)
+            {
+                result = result
+                    .Where(a => a.IsActive == isActive)
+                    .AsQueryable();
+            }
+            return await result.ToListAsync();
+        }
+        public async Task UpdatePublisherOfBooks ()
+        {
+            var books = await _context
+                .Books
+                .Where(b => b.PublisherId == null)
+                .ToListAsync();
+            foreach (var book in books)
+            {
+                book.AuthorId = 1;
+            }
+            _context.Books.UpdateRange(books);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
