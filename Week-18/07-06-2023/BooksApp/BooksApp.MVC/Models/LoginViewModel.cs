ed1475651a74520cf2954889a82933ea362e2904
mehmettalbayrak <112020000+mehmettalbayrak@users.noreply.github.com>
2023-06-07 21:58:22 +0300

    Kullanıcı Giriş

---
+++ Week-18/07-06-2023/BooksApp/BooksApp.MVC/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BooksApp.MVC.Models
+{
+    public class LoginViewModel
+    {
+        [DisplayName("Kullanıcı Adı")]
+        [Required(ErrorMessage = "Kullanıcı Adı alanı boş bırakılmamalıdır.")]
+        public string UserName { get; set; }
+
+        [DisplayName("Şifre")]
+        [Required(ErrorMessage = "Şifre alanı boş bırakılmamalıdır.")]
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
