1557142ec1db617976b5ede5a8003a475b569ff0
mehmettalbayrak <112020000+mehmettalbayrak@users.noreply.github.com>
2023-06-14 21:44:42 +0300

    Create CartController.cs

---
+++ Week-19/13-06-2023/BooksApp/BooksApp.MVC/Controllers/CartController.cs
@@ -0,0 +1,23 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using BooksApp.Business.Abstract;
+using BooksApp.Entity.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksApp.MVC.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly ICartService _cartService;
+        private readonly INotyfService _notify;
+
+        public CartController(UserManager<User> userManager, ICartService cartService, INotyfService notify)
+        {
+            _userManager = userManager;
+            _cartService = cartService;
+            _notify = notify;
+        }
+        public async Task<IActionResult> 
+    }
+}
