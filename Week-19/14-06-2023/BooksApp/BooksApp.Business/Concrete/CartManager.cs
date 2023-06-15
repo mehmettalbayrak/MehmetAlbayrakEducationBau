8efc89e852df69b6841d65c24ebadca13477b5da
mehmettalbayrak <112020000+mehmettalbayrak@users.noreply.github.com>
2023-06-15 21:46:22 +0300

    addtocart

---
+++ Week-19/14-06-2023/BooksApp/BooksApp.Business/Concrete/CartManager.cs
@@ -0,0 +1,43 @@
+﻿using BooksApp.Business.Abstract;
+using BooksApp.Data.Abstract;
+using BooksApp.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksApp.Business.Concrete
+{
+    public class CartManager : ICartService
+    {
+        private ICartRepository _cartRepository;
+
+        public CartManager(ICartRepository cartRepository)
+        {
+            _cartRepository = cartRepository;
+        }
+
+        public Task AddToCart(string userId, int bookId, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Cart> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Cart> GetCartByUserId(string id)
+        {
+            return await _cartRepository.GetCartByUserId(id);
+        }
+
+        public async Task InitializeCart(string userId)
+        {
+            //Cart cart = new Cart { UserId = userId };
+            //await _cartRepository.CreateAsync(cart);
+            await _cartRepository.CreateAsync(new Cart { UserId = userId });
+        }
+    }
+}
---
+++ Week-19/14-06-2023/BooksApp/BooksApp.MVC/Controllers/CartController.cs
@@ -0,0 +1,50 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using BooksApp.Business.Abstract;
+using BooksApp.Entity.Concrete;
+using BooksApp.MVC.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksApp.MVC.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly ICartService _cartManager;
+        private readonly INotyfService _notyf;
+
+        public CartController(UserManager<User> userManager, ICartService cartManager, INotyfService notyf)
+        {
+            _userManager = userManager;
+            _cartManager = cartManager;
+            _notyf = notyf;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userId = _userManager.GetUserId(User);
+            var cart = await _cartManager.GetCartByUserId(userId);
+            CartViewModel model = new CartViewModel
+            {
+                CartId = cart.Id,
+                CartItems = cart.CartItems
+                    .Select(ci => new CartItemViewModel
+                    {
+                        CartItemId = ci.Id,
+                        BookId = ci.Book.Id,
+                        BookName = ci.Book.Name,
+                        BookUrl = ci.Book.Url,
+                        BookImageUrl = ci.Book.ImageUrl,
+                        Price = ci.Book.Price,
+                        Quantity = ci.Quantity
+                    }).ToList()
+            };
+            return View(model);
+        }
+        public async Task<IActionResult> AddToCart(int id)
+        {
+
+            return View();
+        }
+    }
+}
