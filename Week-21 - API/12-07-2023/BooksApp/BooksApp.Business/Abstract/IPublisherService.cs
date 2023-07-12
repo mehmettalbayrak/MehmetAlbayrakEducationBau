32503c590d625ab9eef485b073dbc2056c044783
mehmettalbayrak <112020000+mehmettalbayrak@users.noreply.github.com>
2023-07-12 21:52:37 +0300

    PublisherManager

---
+++ Week-21 - API/12-07-2023/BooksApp/BooksApp.Business/Abstract/IPublisherService.cs
@@ -0,0 +1,22 @@
+﻿using BooksApp.Entity.Concrete;
+using BooksApp.Shared.DTOs;
+using BooksApp.Shared.ResponseDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksApp.Business.Abstract
+{
+    public interface IPublisherService
+    {
+        #region Generic
+        Task<Response<PublisherDto>> GetByIdAsync(int? id);
+        Task<Response<List<PublisherDto>>> GetAllAsync();
+        Task<Response<PublisherDto>> CreateAsync(PublisherCreateDto publisherCreateDto);
+        Task<Response<NoContent>> UpdateAsync(PublisherDto publisherDto);
+        Task<Response<NoContent>> DeleteAsync(int id);
+        #endregion
+    }
+}
---
+++ Week-21 - API/12-07-2023/BooksApp/BooksApp.Shared/DTOs/PublisherDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksApp.Shared.DTOs
+{
+    public class PublisherDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; } = "Türkiye";
+        public string Phone { get; set; }
+        public string Url { get; set; }
+    }
+}
